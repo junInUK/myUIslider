d79273a61de090b0b5975a94c013462bbb02f17d
junwu <junwu2012@hotmail.com>
2021-09-16 07:59:19 +0100

    0.1 version

--- client/main.js
+++ client/main.js
@@ -9,21 +9,49 @@ Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
   console.log('hello on created!');
-  $(document).ready(
-    function()
-    {
-      let slider = document.getElementById("slider");
-      noUiSlider.create(slider, {
-        start: 0,
-        connect: "lower",
-        range: {
-          'min': 0,
-          'max': 100
-        }
-      });
+  $(document).ready(() => {
+    var select = document.getElementById('input-select');
+
+    // Append the option elements
+    for (var i = -20; i <= 40; i++) {
+      var option = document.createElement("option");
+      option.text = i;
+      option.value = i;
+      select.appendChild(option);
     }
-  );
-  
+
+    var html5Slider = document.getElementById('html5');
+
+    noUiSlider.create(html5Slider, {
+      start: [10, 30],
+      connect: true,
+      tooltips: true,
+      range: {
+          'min': -20,
+          'max': 40
+      }
+    });
+    
+    var inputNumber = document.getElementById('input-number');
+
+    html5Slider.noUiSlider.on('update', function (values, handle) {
+      var value = values[handle];
+      if (handle) {
+        inputNumber.value = value;
+      } else {
+        select.value = Math.round(value);
+      }
+    });
+
+    select.addEventListener('change', function () {
+      html5Slider.noUiSlider.set([this.value, null]);
+    });
+
+    inputNumber.addEventListener('change', function () {
+      html5Slider.noUiSlider.set([null, this.value]);
+    });
+
+  });
 });
 
 Template.hello.helpers({
